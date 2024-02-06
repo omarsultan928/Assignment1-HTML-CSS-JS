53e14b566eb470304f42337eeb3a000b00f6f18b
Md. Omar Ali Sultan <omar@ad.enosisbd.com>
2024-02-06 12:34:19 +0600

    Design 2 Pages, html & css

---
+++ functionality.js
@@ -0,0 +1,19 @@
+// const submitButton = document.getElementsByClassName('submit-button')[0];
+// const locationName = document.getElementById('Location_Name');
+// const locationAddress = document.getElementById('Location_Address');
+// const locationRating = document.getElementById('Location_Rating');
+// const locationType = document.getElementById('Location_Type').selectedOptions[0].value;
+// submitButton.addEventListener('click',()=>{
+//     // console.log(submitButton);
+//     console.log(locationName.value);
+//     console.log(locationAddress.value);
+//     console.log(locationRating.value);
+//     console.log(locationType);
+// });
+
+
+function MyFunc()
+{
+    const form = document.getElementById('newTouristSpot');
+    console.log(form);
+}
